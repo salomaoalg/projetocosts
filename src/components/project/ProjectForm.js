e75579d5d8ea4a5cb9193a2b6da6c9fd8f76f186
salomaoalg <sa.salomao@hotmail.com>
2024-05-14 20:35:32 +0100

    db conexao com o select

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,14 +1,52 @@
+import { useEffect, useState } from "react"
 import styles from "./ProjectForm.module.css"
 import Input from "../form/Input"
 import Select from "../form/Select"
 import SubmitButton from "../form/SubmitButton"
 
 function ProjectForm({btnText}){
+    const [categories, setCategories] = useState([])
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    },[])
+
     return(
         <form className={styles.form}>
-            <div><Input type="text" text="Nome do Projeto" name="name" placeholder="Insira o nome do projeto" /></div>
-            <div><Input type="Number" text="Orçamento do Projeto" name="budge" placeholder="Insira o orçamento total" /></div>
-            <Select name="category_id" text="Selecione a categoria"/>
+            <div>
+                <Input 
+                    type="text" 
+                    text="Nome do Projeto" 
+                    name="name" 
+                    placeholder="Insira o nome do projeto" 
+                />
+            </div>
+            
+            <div>
+                <Input 
+                    type="Number" 
+                    text="Orçamento do Projeto" 
+                    name="budge" 
+                    placeholder="Insira o orçamento total" 
+                />
+            </div>
+            
+            <Select 
+                name="category_id" 
+                text="Selecione a categoria"
+                options={categories}
+            />
+           
             <SubmitButton text={btnText}/>
         </form>
     )
